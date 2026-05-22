c33242fb873ac8c56957f689d806bc5b953007ae
Senarin_ <dltpsk03@gmail.com>
2026-05-22 21:06:31 +0900

    Revert "Initial frontend app"
    This reverts commit 4b438d2254f5a8d8a55daa14e7ecd6ee8115dadb.

--- src/core/restorePlaceholders.ts
+++
@@ -1,9 +0,0 @@
-import type { ProtectedPlaceholder } from '../types/paradox'
-
-export function restorePlaceholders(value: string, placeholders: ProtectedPlaceholder[]) {
-  return placeholders.reduce(
-    (restoredValue, placeholder) =>
-      restoredValue.replaceAll(placeholder.token, placeholder.value),
-    value,
-  )
-}
